b83e5c7199a860f9ffa31eb329b1ffb8bfed092a
Hashem Pourallahverdi <h.pourallahverdi@pnmco.info>
2023-05-17 17:16:48 +0330

    app module and routing set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {BoardGameComponent} from './components/board-game/board-game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: 'board-game', component: BoardGameComponent},
+    {path: '**', redirectTo: 'board-game'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {IconsComponent} from './components/icons/icons.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RulesComponent } from './components/rules/rules.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import { ElementComponent } from './components/element/element.component';
+import {BoardGameComponent} from './components/board-game/board-game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IconsComponent,
+    BoardGameComponent,
+    RulesComponent,
+    ElementComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
